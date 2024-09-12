7af6bfbfd3dac5bc78be929158fa54edcbda75a4
sen.wang@dfoad.com <22232647@qq.xom>
2024-09-12 15:52:30 +0400

    http operator and snowflake example

---
+++ dags/snowflake_python_dag.py
@@ -0,0 +1,58 @@
+import logging
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Default args for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 9, 5),
+}
+
+# Define the DAG
+dag = DAG(
+    dag_id="snowflake_query_hook",
+    default_args=default_args,
+    schedule_interval=None,
+    description='Run a simple query in Snowflake using SnowflakeHook',
+    tags=['example'],
+)
+
+# Snowflake query
+query1 = [
+    """SELECT CURRENT_VERSION();"""
+]
+
+# Python function to run using SnowflakeHook
+def run_query_with_hook(**context):
+    dwh_hook = SnowflakeHook(
+        snowflake_conn_id="snowflake_conn_test",
+        extra_conn_params={"account": "ld43643.uae-north.azure"}
+    )
+    result = dwh_hook.get_first("SELECT CURRENT_VERSION()")
+    logging.info("Snowflake Version: %s", result[0])
+
+
+# Define tasks
+with dag:
+    # Task to execute the query using SnowflakeOperator
+    query1_exec = SnowflakeOperator(
+        task_id="snowflake_operator_task",
+        sql=query1,
+        snowflake_conn_id="snowflake_conn_test",
+    )
+
+    # Task to execute the query using SnowflakeHook in a Python function
+    hook_query_task = PythonOperator(
+        task_id="hook_query_task",
+        python_callable=run_query_with_hook,
+    )
+
+# Define task dependencies
+query1_exec >> hook_query_task
