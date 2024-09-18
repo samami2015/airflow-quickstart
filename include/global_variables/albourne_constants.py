514e8ac70691bf6ac65fd4a95c83ab28dfa06959
sen.wang@dfoad.com <22232647@qq.xom>
2024-09-18 06:36:12 +0400

    demo on albourne with example code

---
+++ include/global_variables/albourne_constants.py
@@ -0,0 +1,2 @@
+# tables names
+IN_ALL_FUNDS_TABLE_NAME = 'T_ABR_FACT_ALL_FUNDS'
---
+++ include/global_variables/albourne_input_variables_example.py
@@ -0,0 +1,4 @@
+USERNAME = '<provide your username>'
+PASSWORD = '<provide your password>'
+BASE_URL = 'provider your url'
+CONN_ID_SNOWFLAKE = '<provider your connection id>'
